e54249c1449970ef2840c2f44dc746d154438b1d
MayBaMay <maylis.baschet@gmail.com>
2020-05-13 12:20:28 +0200

    first custom models migrations test objects creation ok

--- QuitSoonApp/models.py
+++ QuitSoonApp/models.py
@@ -1,12 +1,99 @@
 from django.db import models
-
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 
-# class CustomUser(AbstractUser):
-#     email = models.EmailField(
-#         verbose_name='addresse email',
-#         max_length=255,
-#         unique=True,
-#     )
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_start = models.DateField()
+    starting_nb_cig = models.IntegerField()
+
+
+class Paquet(models.Model):
+    TYPE_CIG = [
+        ('IND', 'Cigarettes industrielles'),
+        ('ROL', 'Cigarettes roulées'),
+        ('CIGARES', 'Cigares'),
+        ('CIGARIOS', 'Cigarios'),
+        ('PIPE', 'Pipe'),
+        ('NB', 'Autres(en nb/paquet)'),
+        ('GR', 'Autres(en g/paquet)'),
+    ]
+    UNIT = [
+        ('U', 'Unités'),
+        ('G', 'Grammes'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type_cig = models.CharField(
+        max_length=8,
+        choices=TYPE_CIG,
+        default='IND',
+    )
+    brand = models.CharField(max_length=200)
+    qt_paquet = models.IntegerField(default=20)
+    unit = models.CharField(
+        max_length=1,
+        choices=UNIT,
+        default='U',
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    g_per_cig = models.DecimalField(max_digits=3, decimal_places=1, null=True)
+    display = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "%s %s %s%s" % (self.type_cig, self.brand, self.qt_paquet, self.unit)
+
+
+class ConsoCig(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_cig = models.DateField()
+    time_cig = models.TimeField()
+    paquet = models.ForeignKey(Paquet, on_delete=models.CASCADE, null=True)
+    given = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s %s-%s" % (self.id_user, self.date_conso, self.time_conso)
+
+
+class Alternative(models.Model):
+    TYPE_ALTERNATIVE = [
+        ('Sp', 'Sport'),
+        ('Lo', 'Loisir'),
+        ('So', 'Soin'),
+        ('Su', 'Substitut'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type_alternative = models.CharField(
+        max_length=200,
+        choices=TYPE_ALTERNATIVE,
+        default='Sp',
+    )
+    alternative = models.CharField(max_length=200)
+    nicotine = models.FloatField(null=True)
+
+    class Meta:
+        unique_together = ('type_alternative', 'alternative', 'nicotine')
+
+
+class ConsoAlternative(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_alter = models.DateField()
+    time_alter = models.TimeField()
+    alternative = models.ForeignKey(Alternative, on_delete=models.CASCADE)
+    duration = models.IntegerField(null=True)
+
+
+class Objectif(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    qt = models.IntegerField(unique=True)
+    datetime_creation = models.DateTimeField()
+    datetime_objectif = models.DateTimeField()
+    respected = models.BooleanField(default=False)
+
+
+class Trophee(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nb_cig = models.IntegerField()
+    nb_jour = models.IntegerField()
+
+    class Meta:
+        unique_together = ('nb_cig', 'nb_jour',)
