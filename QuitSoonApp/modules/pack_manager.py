5f9bf3fd8b01392d15814dd388be389dff4f3e88
MayBaMay <maylis.baschet@gmail.com>
2020-05-28 07:51:52 +0200

    change names modules

--- QuitSoonApp/modules/pack_manager.py
+++ QuitSoonApp/modules/pack_manager.py
@@ -10,7 +10,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from ..models import Paquet, ConsoCig
 
-class SavePack:
+class PackManager:
     """class returning an new DB object paquet or False"""
 
     def __init__(self, user, datas):
--- QuitSoonApp/tests/modules/test_smoke_manager.py
+++ QuitSoonApp/tests/modules/test_smoke_manager.py
@@ -8,11 +8,11 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 
 from QuitSoonApp.models import Paquet, ConsoCig
-from QuitSoonApp.modules import SaveSmoke
+from QuitSoonApp.modules import SmokeManager
 
 
-class SaveSmokeTestCase(TestCase):
-    """class testing SaveSmoke """
+class SmokeManagerTestCase(TestCase):
+    """class testing SmokeManager """
 
     def setUp(self):
         """setup tests"""
@@ -57,40 +57,40 @@ class SaveSmokeTestCase(TestCase):
         self.old_smoke_ind_data = {'id_smoke': self.db_smoke_ind.id}
 
     def test_get_request_data(self):
-        """test SaveSmoke.get_request_data method"""
-        smoke = SaveSmoke(self.usertest, self.new_datas_ind)
+        """test SmokeManager.get_request_data method"""
+        smoke = SmokeManager(self.usertest, self.new_datas_ind)
         self.assertEqual(smoke.get_request_data('date_smoke'), datetime.date(2020, 5, 17))
         self.assertEqual(smoke.get_request_data('time_smoke'), datetime.time(13, 15))
         self.assertEqual(smoke.get_request_data('indus_pack_field'), self.db_pack_ind.id)
 
     def tes_get_pack_ind(self):
-        """test SaveSmoke.get_pack method with new smoke datas and given_field=False & type_cig_field='IND'"""
-        smoke = SaveSmoke(self.usertest, self.new_datas_ind)
+        """test SmokeManager.get_pack method with new smoke datas and given_field=False & type_cig_field='IND'"""
+        smoke = SmokeManager(self.usertest, self.new_datas_ind)
         self.assertEqual(smoke.get_pack, self.db_pack_ind)
         self.assertEqual(smoke.paquet, self.db_pack_ind)
 
     def test_get_pack_rol(self):
-        """test SaveSmoke.get_pack method with new smoke datas : given_field=False & type_cig_field='ROL'"""
-        smoke = SaveSmoke(self.usertest, self.new_datas_rol)
+        """test SmokeManager.get_pack method with new smoke datas : given_field=False & type_cig_field='ROL'"""
+        smoke = SmokeManager(self.usertest, self.new_datas_rol)
         self.assertEqual(smoke.get_pack, self.db_pack_rol)
         self.assertEqual(smoke.paquet, self.db_pack_rol)
 
     def test_get_pack__given_cig(self):
-        """test SaveSmoke.get_pack method with new smoke datas and given_field=True"""
+        """test SmokeManager.get_pack method with new smoke datas and given_field=True"""
         self.new_datas_ind['given_field'] = True
-        smoke = SaveSmoke(self.usertest, self.new_datas_ind)
+        smoke = SmokeManager(self.usertest, self.new_datas_ind)
         self.assertEqual(smoke.get_pack, None)
         self.assertEqual(smoke.paquet, None)
 
     def test_get_pack_smoke_id(self):
-        """test SaveSmoke.get_pack method with id_smoke in datas (for delete_smoke view)"""
-        smoke = SaveSmoke(self.usertest, self.old_smoke_ind_data)
+        """test SmokeManager.get_pack method with id_smoke in datas (for delete_smoke view)"""
+        smoke = SmokeManager(self.usertest, self.old_smoke_ind_data)
         self.assertEqual(smoke.get_pack, self.db_pack_ind)
         self.assertEqual(smoke.paquet, self.db_pack_ind)
 
     def test_create_conso_cig(self):
-        """test SaveSmoke.create_conso_cig method with new conso datas"""
-        smoke = SaveSmoke(self.usertest, self.new_datas_ind)
+        """test SmokeManager.create_conso_cig method with new conso datas"""
+        smoke = SmokeManager(self.usertest, self.new_datas_ind)
         smoke.create_conso_cig()
         new_smoke = ConsoCig.objects.filter(user=self.usertest,
                              date_cig=datetime.date(2020, 5, 17),
@@ -101,12 +101,12 @@ class SaveSmokeTestCase(TestCase):
         self.assertEqual(smoke.paquet.id, self.db_pack_ind.id)
 
     def test_create_conso_cig_datas_id_smoke(self):
-        """test SaveSmoke.create_conso_cig method with id_smoke in request"""
-        smoke = SaveSmoke(self.usertest, self.old_smoke_ind_data)
+        """test SmokeManager.create_conso_cig method with id_smoke in request"""
+        smoke = SmokeManager(self.usertest, self.old_smoke_ind_data)
         self.assertEqual(smoke.create_conso_cig(), None)
 
     def test_create_conso_cig_given(self):
-        """ test SaveSmoke.create_conso_cig method with given=True """
+        """ test SmokeManager.create_conso_cig method with given=True """
         new_datas_given_cig = {
             'date_smoke': datetime.date(2020, 7, 15),
             'time_smoke': datetime.time(20, 15),
@@ -114,7 +114,7 @@ class SaveSmokeTestCase(TestCase):
             'rol_pack_field': self.db_pack_rol.id,
             'given_field':True,
             }
-        smoke = SaveSmoke(self.usertest, new_datas_given_cig)
+        smoke = SmokeManager(self.usertest, new_datas_given_cig)
         smoke.create_conso_cig()
         self.assertEqual(smoke.paquet, None)
         new_given_smoke = ConsoCig.objects.filter(user=self.usertest,
@@ -126,13 +126,13 @@ class SaveSmokeTestCase(TestCase):
 
 
     def test_get_conso_cig_id_smoke(self):
-        """test SaveSmoke.conso_cig method with id_smoke in request"""
-        smoke = SaveSmoke(self.usertest, self.old_smoke_ind_data)
+        """test SmokeManager.conso_cig method with id_smoke in request"""
+        smoke = SmokeManager(self.usertest, self.old_smoke_ind_data)
         self.assertEqual(smoke.get_conso_cig, self.db_smoke_ind)
 
     def test_get_conso_cig_new_conso(self):
-        """test SaveSmoke.conso_cig method after creation object with new conso datas"""
-        smoke = SaveSmoke(self.usertest, self.new_datas_ind)
+        """test SmokeManager.conso_cig method after creation object with new conso datas"""
+        smoke = SmokeManager(self.usertest, self.new_datas_ind)
         smoke.create_conso_cig()
         self.assertEqual(smoke.get_conso_cig, ConsoCig.objects.get(
             user=self.usertest,
@@ -143,19 +143,19 @@ class SaveSmokeTestCase(TestCase):
             ))
 
     def test_get_conso_cig_not_created(self):
-        """test SaveSmoke.conso_cig method without creating object with new conso datas"""
-        smoke = SaveSmoke(self.usertest, self.new_datas_ind)
+        """test SmokeManager.conso_cig method without creating object with new conso datas"""
+        smoke = SmokeManager(self.usertest, self.new_datas_ind)
         self.assertEqual(smoke.get_conso_cig, None)
 
     def test_delete_conso_cig(self):
-        """test SaveSmoke.delete_conso method"""
-        smoke = SaveSmoke(self.usertest, self.old_smoke_ind_data)
+        """test SmokeManager.delete_conso method"""
+        smoke = SmokeManager(self.usertest, self.old_smoke_ind_data)
         smoke.delete_conso_cig()
         filter_conso = ConsoCig.objects.filter(user=self.usertest, paquet=self.db_pack_ind.id)
         self.assertFalse(filter_conso.exists())
 
     def test_delete_conso_cig_given_cig(self):
-        """test SaveSmoke.delete_conso method with cig == given cig"""
+        """test SmokeManager.delete_conso method with cig == given cig"""
         db_smoke_given = ConsoCig.objects.create(
             user=self.usertest,
             date_cig=datetime.date(2020, 5, 17),
@@ -163,7 +163,7 @@ class SaveSmokeTestCase(TestCase):
             paquet=None,
             )
         data = {'id_smoke': db_smoke_given.id}
-        smoke = SaveSmoke(self.usertest, data)
+        smoke = SmokeManager(self.usertest, data)
         smoke.delete_conso_cig()
         filter_conso = ConsoCig.objects.filter(user=self.usertest, id=db_smoke_given.id)
         self.assertFalse(filter_conso.exists())
