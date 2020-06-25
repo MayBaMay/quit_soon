25e2f0f962ed76c411ba8e83c5b1dd391d9d09a0
MayBaMay <maylis.baschet@gmail.com>
2020-06-25 15:36:20 +0200

    module chexk_trophees for nb_cig=0 good te go

--- QuitSoonApp/modules/check_trophees.py
+++ QuitSoonApp/modules/check_trophees.py
@@ -7,6 +7,8 @@ import calendar
 import pandas as pd
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.db import transaction
+
 
 from QuitSoonApp.models import Trophee
 
@@ -15,34 +17,43 @@ class Trophee_checking:
 
     def __init__(self, stats):
         self.stats = stats
-        # self.df = smoking_values_per_dates_with_all_dates_df(self.all_dates, self.values_per_dates)
-        # self.user_futur_month_trophees = check_trophees_to_be_complete([30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330])
-        # self.user_futur_days_trophees = self.check_trophees_to_be_complete([1, 2, 3, 4, 7, 10, 15, 20, 25])
-        # self.trophee_to_create = self.check_days_trophees + self.check_month_trophees
+        self.df = self.smoking_values_per_dates_with_all_dates_df(self.all_dates, self.values_per_dates)
+        self.user_futur_month_trophees = self.check_trophees_to_be_completed([30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330])
+        self.user_futur_days_trophees = self.check_trophees_to_be_completed([1, 2, 3, 4, 7, 10, 15, 20, 25])
+        self.trophees_to_create = self.check_days_trophees + self.check_month_trophees
 
     @property
     def values_per_dates(self):
-        """ get smoking values in a dataframe """
+        """ get count cig smoked (col nb_cig) per dates(index) in DataFrame """
         qs = self.stats.user_conso_full_days.values()
         data_cig = pd.DataFrame(qs)
         # combine stae_cig and time_cig into a datetime column
         data_cig['date'] = data_cig.apply(lambda r : dt.combine(r['date_cig'],r['time_cig']),1)
         # get nb_cig per date sorted
         nb_cig_per_date_serie = data_cig.date.dt.date.value_counts().sort_index()
-        # rename serie to nb_cig
+        # rename serie to nb_cig and index to date
         nb_cig_per_date_serie.rename("nb_cig", inplace=True)
         nb_cig_per_date_serie.index.name = 'date'
+        # convert date to datetime
+        nb_cig_per_date_serie.index = pd.to_datetime(nb_cig_per_date_serie.index)
         # convert serie to dataframe
         return nb_cig_per_date_serie.to_frame()
 
     @property
     def all_dates(self):
+        """get all passed dates, index of empty dataframe"""
         ### get dataframe with all dates ###
         all_days_df = pd.DataFrame(self.stats.list_dates, columns=['date']).set_index('date')
+        # take out the current day (only full pasts days)
+        all_days_df.drop(all_days_df.tail(1).index,inplace=True)
+        # convert date to datetime
         all_days_df.index = pd.to_datetime(all_days_df.index)
         return all_days_df
 
     def smoking_values_per_dates_with_all_dates_df(self, all_days_df, nb_cig_per_date_df):
+        """
+        get dataframe with all passed dates(index) and count cig per dates (col nb_cig)
+        """
         # concats in dataframe and format as needed
         nb_cig_per_date_df = pd.concat([all_days_df, nb_cig_per_date_df], axis=1)
         nb_cig_per_date_df.reset_index(inplace=True)
@@ -52,73 +63,102 @@ class Trophee_checking:
 
     @property
     def get_nans_occurence(self):
-        # get NaNs occurence in nb_cig_per_date_df
+        """ get NaNs occurence in dataframe """
         return self.df.nb_cig.isnull().groupby(self.df.nb_cig.notnull().cumsum()).sum()
 
-    # check if trophee already exists and remove it from trophee to succeed list
-    def check_trophees_to_be_complete(self, list_trophees):
+
+    def check_trophees_to_be_completed(self, list_trophees):
+        """get trophees user didn't completed yet in a list"""
+        # check if trophee already exists and get trophee not yet succeeded in a list
         final_list = []
         for trophee in list_trophees:
-            if not Trophee.objects.filter(user=user, nb_cig=0, nb_jour=trophee).exists():
+            if not Trophee.objects.filter(user=self.stats.user, nb_cig=0, nb_jour=trophee).exists():
                 final_list.append(trophee)
         return final_list
 
     @property
     def check_days_trophees(self):
+        """
         ##################### non smoking days trophees ######################################
-        # for element in occurence de NaNs, check if >= element in trophee to succeed list
-        # break it as soon as list completly checked
+        for element in occurence de NaNs, check if >= element in trophee to succeed list
+        break it as soon as list completly checked
+        """
         trophee_to_create = []
         while True:
             if self.user_futur_days_trophees:
                 for element in self.get_nans_occurence:
                     for trophee in self.user_futur_days_trophees:
-                        if element >= trophee:
-                            trophee_to_create.append(trophee)
-                            if trophee == self.user_futur_days_trophees[-1]:
-                                break
+                        if not trophee in trophee_to_create:
+                            if element >= trophee:
+                                trophee_to_create.append(trophee)
+                                if trophee == self.user_futur_days_trophees[-1]:
+                                    break
             break
         return trophee_to_create
 
     @property
     def parse_smoking_month(self):
+        """
         ##################### non smoking month trophees ######################################
-        # month trophees are saved as %30
-        ### for each month check if not smoked ###
-
+        for each month check if full and not smoking (True), else False
+        """
         if self.user_futur_month_trophees:
             non_smoking_month = []
-            for index, value in nb_cig_per_date_df.date.dt.month.value_counts().sort_index().items():
-                # if full month
-                if value == calendar.monthrange(2020, index)[1]:
-                    # Get True if all data in this month are NaNs
-                    filter_month = nb_cig_per_date_df[(nb_cig_per_date_df.date.dt.month == index)].isnull().values.any()
-                    if filter_month:
-                        ## one month without smoking
-                        non_smoking_month.append(True)
-                        print(index, value, True)
+            # proceed year after year to get appropriate calendar
+            for year in self.df.date.dt.year.drop_duplicates().tolist():
+                df_year = self.df[self.df.date.dt.year == year]
+                for index, value in df_year.date.dt.month.value_counts().sort_index().items():
+                    # if full month
+                    if value == calendar.monthrange(year, index)[1]:
+                        # Get True if all data in this month are NaNs
+                        nb_Nans_in_month = df_year[(df_year.date.dt.month == index)].isnull().sum().nb_cig
+                        total_rows_in_month = df_year[(df_year.date.dt.month == index)].shape[0]
+                        if nb_Nans_in_month == total_rows_in_month:
+                            ## one month without smoking
+                            non_smoking_month.append(True)
+                        else:
+                            non_smoking_month.append(False)
                     else:
                         non_smoking_month.append(False)
-                        print(index, value, False)
-                else:
-                    non_smoking_month.append(False)
-                    print(index, value, False)
-        return non_smoking_month
+            return non_smoking_month
 
     @property
     def check_month_trophees(self):
+        """check if completed trophees months without smoking"""
         trophee_to_create = []
         non_smoking_month = self.parse_smoking_month
-        while True:
-            if self.user_futur_month_trophees:
-                for trophee in self.user_futur_month_trophees:
-                    compared_month = int((trophee / 30) - 1)
-                    try:
-                        if any(non_smoking_month[i]==non_smoking_month[i+compared_month] for i in range(len(non_smoking_month)-1)):
+        # follow only if user still have trophees to complete
+        if self.user_futur_month_trophees:
+            for trophee in self.user_futur_month_trophees:
+                while True:
+                    # month trophees are %30
+                    compared_month = range(int((trophee / 30) - 1))
+                    # for each bool non_smoking_month => True if full month and no smoking
+                    # compare with following bool
+                    for i in range(len(non_smoking_month)):
+                        compare = [non_smoking_month[i]]
+                        n = 0
+                        # based on trophee compared following data would be different size
+                        for month in compared_month:
+                            try:
+                                n+=1
+                                compare.append(non_smoking_month[i+n])
+                            except IndexError:
+                                # comparing data out of list index, pass next trophee checking
+                                break
+                        if not False in compare:
+                            # only full month and non smoked, break to pass next trophee checking
                             trophee_to_create.append(trophee)
-                    except IndexError: #no enough month passed to get trophees
-                        pass
-                    if trophee == self.user_futur_month_trophees[-1]:
-                        break
-            break
+                            break
+                    # end index in non_smoking_month, pass next trophee checking
+                    break
         return trophee_to_create
+
+    def create_trophees_no_smoking(self):
+        for trophee in self.trophees_to_create:
+            try:
+                with transaction.atomic():
+                    Trophee.objects.create(user=self.stats.user, nb_cig=0, nb_jour=trophee)
+            except IntegrityError:
+                # method already called
+                pass
--- QuitSoonApp/tests/modules/test_check_trophees.py
+++ QuitSoonApp/tests/modules/test_check_trophees.py
@@ -11,7 +11,7 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.test import TestCase
 
-from QuitSoonApp.modules.check_trophees import Trophee_checking
+from QuitSoonApp.modules import Trophee_checking
 from QuitSoonApp.models import (
     UserProfile,
     Paquet, ConsoCig,
@@ -19,10 +19,13 @@ from QuitSoonApp.models import (
     )
 from QuitSoonApp.modules import SmokeStats
 
-from ..MOCK_DATA import (
+from QuitSoonApp.tests.MOCK_DATA import (
     Create_packs, Create_smoke,
-    row_paquet_data, fake_smoke
+    row_paquet_data, fake_smoke_for_trophees
     )
+
+# user = User.objects.get(username='maykimay')
+# stats = SmokeStats(user, datetime.date(2020, 7, 20))
 
 class CheckTropheeTestCase(TestCase):
     """class testing Create_smoke """
@@ -38,14 +41,165 @@ class CheckTropheeTestCase(TestCase):
             )
         self.packs = Create_packs(self.user, row_paquet_data)
         self.packs.populate_db()
-        self.smoke = Create_smoke(self.user, fake_smoke)
+        self.smoke = Create_smoke(self.user, fake_smoke_for_trophees)
         self.smoke.populate_db()
-        stats = SmokeStats(self.user, datetime.date(2020, 7, 20))
+        stats = SmokeStats(self.user, datetime.date(2020, 12, 31))
         self.check_prophee = Trophee_checking(stats)
 
     def test_values_per_dates(self):
+        """
+        test method values_per_dates
+        get count cig smoked (col nb_cig) per dates(index) in DataFrame
+        """
         df = self.check_prophee.values_per_dates
-        df.index = pd.to_datetime(df.index)
+        self.assertEqual(df.columns.values.tolist(), ['nb_cig'])
+        self.assertEqual(df.shape, (7, 1))
         self.assertEqual(df.loc['2020-06-19', 'nb_cig'], 19)
         self.assertEqual(df.loc['2020-06-20', 'nb_cig'], 14)
-        self.assertEqual(df.shape[0], 2)
+        self.assertEqual(df.loc['2020-06-22', 'nb_cig'], 1)
+        self.assertEqual(df.loc['2020-06-26', 'nb_cig'], 1)
+        self.assertEqual(df.loc['2020-07-05', 'nb_cig'], 1)
+        self.assertEqual(df.loc['2020-07-19', 'nb_cig'], 1)
+        self.assertEqual(df.loc['2020-10-19', 'nb_cig'], 1)
+        # current day not in df
+        with self.assertRaises(KeyError) as raises:
+            df.loc['2020-12-31', 'nb_cig']
+
+    def test_all_dates(self):
+        """
+        test method all_dates
+        get all passed dates, index of empty dataframe
+        """
+        df = self.check_prophee.all_dates
+        self.assertEqual(df.columns.values.tolist(), [])
+        sum_days = 12+31+31+30+31+30+30
+        self.assertEqual(df.shape, (sum_days, 0))
+        self.assertEqual(df.index[0], dt(2020, 6, 19))
+        self.assertEqual(df.index[1], dt(2020, 6, 20))
+        self.assertEqual(df.index[2], dt(2020, 6, 21))
+        self.assertEqual(df.index[7], dt(2020, 6, 26))
+        self.assertEqual(df.index[16], dt(2020, 7, 5))
+        self.assertEqual(df.index[30], dt(2020, 7, 19))
+        # current day not in df
+        self.assertEqual(df.index[-1], dt(2020, 12, 30))
+        self.assertEqual(df.columns.values.tolist(), [])
+
+    def test_smoking_values_per_dates_with_all_dates_df(self):
+        """
+        test method smoking_values_per_dates_with_all_dates_df
+        get dataframe with all passed dates(index) and count cig per dates (col nb_cig)
+        """
+        all_days_df = self.check_prophee.all_dates
+        nb_cig_per_date_df = self.check_prophee.values_per_dates
+        df = self.check_prophee.smoking_values_per_dates_with_all_dates_df(all_days_df, nb_cig_per_date_df)
+
+        self.assertEqual(df.columns.values.tolist(), ['date', 'nb_cig'])
+        sum_days = 12+31+31+30+31+30+30
+        self.assertEqual(df.shape, (sum_days, 2))
+        self.assertEqual(df[(df['date'] == '2020-06-19')].nb_cig.values, [19.0])
+        self.assertEqual(df[(df['date'] == '2020-06-20')].nb_cig.values, [14.0])
+        self.assertEqual(df[(df['date'] == '2020-06-22')].nb_cig.values, [1.0])
+        self.assertEqual(df[(df['date'] == '2020-06-26')].nb_cig.values, [1.0])
+        self.assertEqual(df[(df['date'] == '2020-07-05')].nb_cig.values, [1.0])
+        self.assertEqual(df[(df['date'] == '2020-07-19')].nb_cig.values, [1.0])
+        self.assertEqual(df[(df['date'] == '2020-10-19')].nb_cig.values, [1.0])
+        # nb_cig col = NaN if no smoke
+        self.assertTrue(pd.isna(df[(df['date'] == '2020-07-01')].nb_cig.values))
+        self.assertTrue(pd.isna(df[(df['date'] == '2020-07-06')].nb_cig.values))
+        self.assertTrue(pd.isna(df[(df['date'] == '2020-07-18')].nb_cig.values))
+        # only 2 days smoke so NaNs nb_cig = count rows - days smoke
+        self.assertEqual(pd.isna(df.nb_cig).sum(), sum_days-7)
+
+    def test_get_nans_occurence(self):
+        """
+        test method get_nans_occurence
+        get NaNs occurence in dataframe
+        """
+        s = self.check_prophee.get_nans_occurence
+        print(s)
+        # 2020-06-21 = 1 days non smoke
+        self.assertTrue(1.0 in s.values)
+        # from 2020-06-23 to 2020-06-25 = 3 days non smoke
+        self.assertTrue(3.0 in s.values)
+        # from 2020-06-27 to 2020-07-04 = 8 days non smoke
+        self.assertTrue(8.0 in s.values)
+        # from 2020-07-06 to 2020-07-18 = 13 days non smoke
+        self.assertTrue(13.0 in s.values)
+        # from 2020-07-20 to 2020-10-18
+        self.assertTrue(91.0 in s.values)
+        # from 2020-10-19 to 2020-12-30
+        self.assertTrue(72.0 in s.values)
+        # sum occurence in s = sum non smoking days
+        self.assertEqual(s.sum(), 1+3+8+13+91+72)
+
+    def test_check_trophees_to_be_completed(self):
+        """
+        test method check_trophees_to_be_completed
+        get trophees user didn't completed yet in a list
+        """
+        # trophee checking never done
+        unparsed_list = [1, 2, 3, 4, 5, 7, 10, 15, 30, 60]
+        trophees = self.check_prophee.check_trophees_to_be_completed(unparsed_list)
+        self.assertEqual(trophees, [1, 2, 3, 4, 5, 7, 10, 15, 30, 60])
+        # trophee checking already done ones and Trophees exits in Trophee table
+        Trophee.objects.create(user=self.user, nb_cig=0, nb_jour=1)
+        Trophee.objects.create(user=self.user, nb_cig=0, nb_jour=2)
+        trophees = self.check_prophee.check_trophees_to_be_completed(unparsed_list)
+        self.assertEqual(trophees, [3, 4, 5, 7, 10, 15, 30, 60])
+
+    def test_check_days_trophees(self):
+        """
+        test method check_days_trophees
+        for element in occurence de NaNs, check if >= element in trophee to succeed list
+        return list of trophees to create
+        """
+        # first checking
+        trophees_to_create = self.check_prophee.check_days_trophees
+        self.assertEqual(trophees_to_create, [1, 2, 3, 4, 7, 10, 15, 20, 25])
+
+    def test_parse_smoking_month(self):
+        """
+        test method parse_smoking_month
+        for each month check if full and not smoking (True), else False
+        """
+        month_parsed = self.check_prophee.parse_smoking_month
+        self.assertEqual(month_parsed, [False, False, True, True, False, True, False])
+
+    def test_check_month_trophees(self):
+        """
+        test method check_month_trophees
+        check if completed trophees months without smoking
+        """
+        trophees_to_create = self.check_prophee.check_month_trophees
+        self.assertEqual(trophees_to_create, [30, 60])
+
+    def test_trophee_to_create(self):
+        """
+        test attribute trophees_to_create
+        get all trophees completed during checking
+        """
+        self.assertEqual(self.check_prophee.trophees_to_create, [1, 2, 3, 4, 7, 10, 15, 20, 25, 30, 60])
+
+    def test_create_trophees_no_smoking(self):
+        """
+        test method create_trophees_no_smoking
+        With list trophees to create, create trophees in database
+        """
+        self.check_prophee.create_trophees_no_smoking()
+        trophees = Trophee.objects.filter(user=self.user, nb_cig=0)
+        self.assertTrue(trophees.exists())
+        self.assertTrue(trophees.count(), len(self.check_prophee.trophees_to_create))
+        self.assertTrue(trophees.get(nb_jour=1))
+        self.assertTrue(trophees.get(nb_jour=2))
+        self.assertTrue(trophees.get(nb_jour=3))
+        self.assertTrue(trophees.get(nb_jour=4))
+        self.assertTrue(trophees.get(nb_jour=7))
+        self.assertTrue(trophees.get(nb_jour=10))
+        self.assertTrue(trophees.get(nb_jour=15))
+        self.assertTrue(trophees.get(nb_jour=20))
+        self.assertTrue(trophees.get(nb_jour=25))
+        self.assertTrue(trophees.get(nb_jour=30))
+        self.assertTrue(trophees.get(nb_jour=60))
+        self.check_prophee.create_trophees_no_smoking()
+        trophees = Trophee.objects.filter(user=self.user, nb_cig=0)
+        self.assertTrue(trophees.get(nb_jour=1))
