a4b9ece3eb15149bb9f95556dbc7c1322e331b76
MayBaMay <maylis.baschet@gmail.com>
2020-06-27 12:26:39 +0200

    reset password in place but ugly

---
+++ QuitSoonApp/tests/test_reset_password.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""tests reset password by user """
+
+from django.test import TestCase, TransactionTestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.core import mail
+
+class ResetPasswordTransactionTestCase(TransactionTestCase):
+
+    def setUp(self):
+        """setup tests"""
+        self.user1 = User.objects.create_user(
+            'registerTestUser', 'test@test.com', 'testpassword')
+
+    def test_reset_password(self):
+        """test reset password view in get method"""
+
+        # get password_reset
+        response = self.client.get(reverse('QuitSoonApp:password_reset'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/password_reset_form.html')
+
+        # post password_reset wrong email
+        response = self.client.post('/password_reset/', {'email': 'not_a_real_email@email.com'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(len(mail.outbox), 0)
+
+        # post password_reset email found
+        response = self.client.post(reverse('password_reset'), {'email': 'test@test.com'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertIn("http://", mail.outbox[0].body)
+        self.assertEqual(mail.outbox[0].subject, 'NicotineKill réinitialisation du mot de passe')
+        # test redirect to password_reset_done
+        self.assertRedirects(response, '/accounts/password_reset/done/')
+
+        # get password_reset_confirm
+        uid = response.context[0]['uid']
+        url = mail.outbox[0].body.split('http://testserver')[1].split('\n', 1)[0]
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/accounts/reset/'+uid+'/set-password/')
+
+        # post valid password_reset_confirm
+        data = {'new_password1': 'newpasswordforTest',
+                'new_password2': 'newpasswordforTest'}
+        response = self.client.post('/accounts/reset/'+uid+'/set-password/', data=data)
+        self.assertEqual(response.status_code, 302)
+        # test redirect to password_reset_complete
+        self.assertRedirects(response, '/accounts/reset/done/')
+
+        self.assertTrue(
+            get_user_model()._default_manager.get().check_password('newpasswordforTest'))
