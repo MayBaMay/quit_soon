4a11e6569b0560a674540ee0db93a229eef5d20c
MayBaMay <maylis.baschet@gmail.com>
2020-09-02 17:41:03 +0200

    collectstatic

--- static/QuitSoonApp/js/quitsoon.js
+++ static/QuitSoonApp/js/quitsoon.js
@@ -1,16 +1,13 @@
 (function ($) {
   "use strict"; // Start of use strict
 
-  // function get_time_zone_offset( ) {
-  // var current_date = new Date();
-  // return parseInt(-current_date.getTimezoneOffset() / 60);
-  // }
-  // console.log(get_time_zone_offset());
-
-
-
-
   ///////////////////show password//////////////////////
+
+  function get_time_zone_offset( ) {
+  var current_date = new Date();
+  return parseInt(-current_date.getTimezoneOffset() / 60);
+  }
+  console.log(get_time_zone_offset());
 
   $(".pass-status").on("click", function (e) {
     var passwordInput = $(".password-field");
